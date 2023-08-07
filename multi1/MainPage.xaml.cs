ddefae480e4582cf1656cf7cc30815a726a971ed
cagri <cyucel@gmail.com>
2023-08-07 17:12:33 +0100

    Initial

---
+++ multi1/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace multi1;
+
+public partial class MainPage : ContentPage
+{
+    IDispatcherTimer Timer1;
+    public MainPage()
+	{
+		InitializeComponent();
+
+		
+		Timer1 = Dispatcher.CreateTimer();
+		Timer1.Interval = TimeSpan.FromMilliseconds(20);
+		Timer1.Tick += FlipCard;
+
+        //Card1.AnchorX = 1;
+		Timer1.Start();
+
+    }
+
+	private void FlipCard(object sender, EventArgs e)
+	{
+		Card1.RotationY += 0.5;
+		if (Card1.RotationY == 50)
+			Timer1.Stop();
+	}
+
+
+}
+
+
